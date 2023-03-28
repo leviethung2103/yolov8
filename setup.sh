fe25598f4109b879ef069c09ae4ff6fc594cb14c
leviethung2103 <leviethung1280@gmail.com>
2023-03-28 21:17:40 +0700

    Update yolov5

--- setup.sh
+++ setup.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 # Install the libraries ....
 apt-get update -y
-apt-get install htop libgl1  wget unzip nano zip gcc htop -y
+apt-get install htop libgl1  wget unzip nano zip gcc htop tree -y
 pip install gdown 
+
+unzip yolov5.zip
+
+# install rclone 
+cd /root/
+wget https://downloads.rclone.org/rclone-current-linux-amd64.zip
+unzip rclone-current-linux-amd64.zip
+sudo cp rclone-v*-linux-amd64/rclone /usr/sbin/
+rm -rf rclone-*
+
